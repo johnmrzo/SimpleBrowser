f470cfcd877b906b57b7891bb793ec57dbdf76ac
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-10-23 08:38:35 -0400

    stared new project

---
+++ SimpleBrowser/SimpleBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBrowserApp.swift
+//  SimpleBrowser
+//
+//  Created by Bakhtiyorjon Mirzajonov on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
